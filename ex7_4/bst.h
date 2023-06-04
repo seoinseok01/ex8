19b25a01e7233253bf354c9f0955ed20c42e13d8
seoinseok01 <insuk010108@gmail.com>
2023-06-04 22:58:36 +0900

    init commit

--- ex7_4/bst.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "node.h"
-
-treeNode* searchBST(treeNode* root, element x);
-treeNode* insertBSTNode(treeNode* p, element x);
-void deleteBSTNode(treeNode* root, element key);
